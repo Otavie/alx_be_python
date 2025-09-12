ca036d076509b56608ed970e7593fc9dc9894ba4
Otavie <otavieokuoyo@gmail.com>
2025-09-12 06:03:29 +0100

    First Commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 15
+print("Addition of ", number1, " and ", number2, " is ", number1 + number2)
+print("Subtraction of ", number1, " and ", number2, " is ", number1 - number2)
+print("Multiplication of ", number1, " and ", number2, " is ", number1 * number2)
+print("Division of ", number1, " and ", number2, " is ", number1 / number2)
